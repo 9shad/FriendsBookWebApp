8a34bb57db17fe2733b39723cb44c0d0aff0f112
Naushad Sindhwani <sindhwani.naushad@gmail.com>
2019-04-20 22:28:34 -0500

    Added Messages and bugfixes

--- src/java/com/friendsbook/MessageAction.java
+++ src/java/com/friendsbook/MessageAction.java
@@ -5,7 +5,9 @@
  */
 package com.friendsbook;
 
+import com.friendsbook.DAO.NotificationDAO;
 import com.friendsbook.DAO.UserMessageDAO;
+import com.friendsbook.pojo.MessageNotification;
 import com.friendsbook.pojo.UserMessage;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -31,7 +33,7 @@ public class MessageAction implements Serializable {
     private String message;
     List<UserMessage> historyMessages;
     private String toUserId;
-    private String htmlOutput;
+    private String htmlOutput = "<div class=\"p-2 mb-1 bg-secondary text-white\" style=\"width:100%;border-radius: 25px\">Click on a user to start conversation!</div>";
     
     public String getMessage() {
         return message;
@@ -50,6 +52,8 @@ public class MessageAction implements Serializable {
     }
     
     public void sendMessage(String fromUserId){
+        if(toUserId == null || toUserId.isEmpty())
+            return;
         UserMessage userMessage = new UserMessage();
         userMessage.setFromUser(fromUserId);
         userMessage.setToUser(this.toUserId);
@@ -57,6 +61,17 @@ public class MessageAction implements Serializable {
         if(UserMessageDAO.sendMessage(userMessage)) {
             historyMessages.add(userMessage);
             //append message to  html output
+            StringBuffer sb = new StringBuffer();
+            sb.append("<div class=\"p-2 mb-2 bg-secondary text-white ml-auto\" style=\"width:40%;border-radius: 25px\">");
+            sb.append(userMessage.getMsgDescription());
+            sb.append("<br/><footer class=\"blockquote-footer text-white\" style=\"margin-left:48%\">");
+            sb.append(userMessage.getTimeStamp());
+            sb.append("</footer>");
+            sb.append("</div>");
+            if(historyMessages.size()>1)
+                htmlOutput += sb.toString();
+            else
+                htmlOutput = sb.toString();
         }else {
             FacesContext.getCurrentInstance().addMessage("", new FacesMessage("Oops! something went wrong, please try again."));
         }
@@ -69,7 +84,7 @@ public class MessageAction implements Serializable {
     
     public void loadHistoryMessage(String fromUserId, String toUserId) {
         this.toUserId = toUserId;
-        if(historyMessages!=null || !historyMessages.isEmpty()){
+        if(historyMessages!=null && !historyMessages.isEmpty()){
             historyMessages.clear();
         }
         historyMessages = UserMessageDAO.getHistoryMessages(fromUserId, toUserId);
@@ -81,14 +96,39 @@ public class MessageAction implements Serializable {
             htmlOutput = "";
             StringBuilder sb = new StringBuilder();
             for(UserMessage message : historyMessages){
-                
+                if(toUserId.equals(message.getToUser())){ 
+                    //indicates loggedin user has received this message to toUser
+                    sb.append("<div class=\"p-2 mb-2 bg-secondary text-white ml-auto\" style=\"width:40%;border-radius: 25px\">");
+                    sb.append(message.getMsgDescription());
+                    sb.append("<br/><footer class=\"blockquote-footer text-white\" style=\"margin-left:50%\">");
+                    sb.append(message.getTimeStamp());
+                    sb.append("</footer>");
+                    sb.append("</div>");
+                }else{
+                    sb.append("<div class=\"p-2 mb-2 bg-info text-white mr-auto\" style=\"width:40%;border-radius: 25px\">");
+                    sb.append(message.getMsgDescription());
+                    sb.append("<br/><footer class=\"blockquote-footer text-white\" style=\"margin-left:50%\">");
+                    sb.append(message.getTimeStamp());
+                    sb.append("</footer>");
+                    sb.append("</div>");
+                }
             }
+            if(sb.length() > 0)
+                htmlOutput = sb.toString();
+            else
+                htmlOutput = "<div class=\"p-1 mb-1 bg-secondary text-white\" style=\"width:100%;border-radius: 25px\"> No Messages to display</div>";
         }
     }
     
-    public String loadMessage(String fromUserId, String toUserId){
-        this.loadHistoryMessage(fromUserId, toUserId);
-        return "messages.xhtml";
+    public String loadMessage(String fromUserId, String toUserId,MessageNotification notification, List<MessageNotification> messageNotifications){
+        if(NotificationDAO.processNotification(notification.getId())){
+            messageNotifications.remove(notification);
+            this.loadHistoryMessage(fromUserId, toUserId);
+            return "messages.xhtml";
+        }else{
+            FacesContext.getCurrentInstance().addMessage("", new FacesMessage("Oops! something went wrong, please try again!"));
+            return "";
+        }
     }
     
 }
